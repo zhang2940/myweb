377c67302acc1340f2834e59737877ab9e19e65e
张涛涛 <2940138921@qq.com>
2022-01-10 18:07:45 +0800

    图片上传，日志

--- src/main/java/com/example/myweb/MyWebApplication.java
+++ src/main/java/com/example/myweb/MyWebApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
-@EnableTransactionManagement
 public class MyWebApplication {
 
     public static void main(String[] args) {
